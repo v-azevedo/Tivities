03614e5b676541f49e493500162e2a171dfaf9de
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-16 18:53:02 -0300

    Include db context service with basic configuration for sqlite

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
