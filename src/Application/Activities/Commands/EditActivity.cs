3443557650661d0679c0dcaaf308a55dec562895
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-17 20:18:32 -0300

    feat: implement the create/edit activity commands

---
+++ src/Application/Activities/Commands/EditActivity.cs
@@ -0,0 +1,28 @@
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities.Commands;
+
+public class EditActivity
+{
+    public class Command : IRequest
+    {
+        public required Activity Activity { get; set; }
+
+
+    }
+
+    public class Handler(AppDbContext context) : IRequestHandler<Command>
+    {
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken)
+                ?? throw new Exception("Activity not found");
+
+            activity.Title = request.Activity.Title;
+
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
