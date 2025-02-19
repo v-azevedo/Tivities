8fa1a0f19e0a25f0e6669b86e9d8b3c917902cb6
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-19 15:01:20 -0300

    feat: implements routes

---
+++ client/src/app/router/Routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import App from "../layout/App";
+import HomePage from "../../features/home/HomePage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
