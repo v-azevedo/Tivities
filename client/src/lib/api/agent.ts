b053014c43e80989973c0f9f7b0fe55d7d0f6ef8
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-19 13:07:30 -0300

    fix: missing await for the sleep function

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -12,7 +12,7 @@ const agent = axios.create({
 
 agent.interceptors.response.use(async (response) => {
   try {
-    sleep(1000);
+    await sleep(1000);
     return response;
   } catch (error) {
     console.log(error);
