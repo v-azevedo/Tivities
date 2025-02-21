f5eba2976b45d3cd9719fa6b904bb35576ec86ce
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-21 17:33:06 -0300

    feat: improve design and prepare for functionalities

---
+++ client/src/features/activities/details/ActivityDetailsInfo.tsx
@@ -0,0 +1,43 @@
+import { Info, CalendarToday, Place } from "@mui/icons-material";
+import { Paper, Grid2, Typography, Divider } from "@mui/material";
+
+type Props = {
+  activity: Activity;
+};
+
+const ActivityDetailsInfo = ({ activity }: Props) => {
+  return (
+    <Paper sx={{ mb: 2 }}>
+      <Grid2 container alignItems="center" pl={2} py={1}>
+        <Grid2 size={1}>
+          <Info color="info" fontSize="large" />
+        </Grid2>
+        <Grid2 size={11}>
+          <Typography>{activity.description}</Typography>
+        </Grid2>
+      </Grid2>
+      <Divider />
+      <Grid2 container alignItems="center" pl={2} py={1}>
+        <Grid2 size={1}>
+          <CalendarToday color="info" fontSize="large" />
+        </Grid2>
+        <Grid2 size={11}>
+          <Typography>{activity.date}</Typography>
+        </Grid2>
+      </Grid2>
+      <Divider />
+
+      <Grid2 container alignItems="center" pl={2} py={1}>
+        <Grid2 size={1}>
+          <Place color="info" fontSize="large" />
+        </Grid2>
+        <Grid2 size={11}>
+          <Typography>
+            {activity.venue}, {activity.city}
+          </Typography>
+        </Grid2>
+      </Grid2>
+    </Paper>
+  );
+};
+export default ActivityDetailsInfo;
