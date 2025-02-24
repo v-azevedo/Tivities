1a4b70523a39eb25a1a5d31a6d8c6168d44028ee
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-24 20:17:03 -0300

    feat: style the homepage

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,10 +1,46 @@
-import { Container, Typography } from "@mui/material";
+import { Group } from "@mui/icons-material";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { Link } from "react-router";
 
 const HomePage = () => {
   return (
-    <Container sx={{ mt: 3 }}>
-      <Typography variant="h3">Homepage</Typography>
-    </Container>
+    <Paper
+      sx={{
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        gap: 6,
+        alignItems: "center",
+        alignContent: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundImage:
+          "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignContent: "center",
+          alignItems: "center",
+          gap: 2,
+          color: "white",
+        }}
+      >
+        <Group sx={{ height: 110, width: 110 }} />
+        <Typography variant="h1">Tivities</Typography>
+      </Box>
+      <Typography variant="h2">Welcome to Tivities</Typography>
+      <Button
+        component={Link}
+        to="/activities"
+        size="large"
+        variant="contained"
+        sx={{ height: 80, borderRadius: 4, fontSize: "1.5rem" }}
+      >
+        Take me to the activities!
+      </Button>
+    </Paper>
   );
 };
 export default HomePage;
