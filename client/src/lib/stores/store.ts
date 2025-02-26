6a945a73ca6de9085d4dd803c36336b4a5d75ae2
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-26 12:21:05 -0300

    chore: remove mobx try out code

--- client/src/lib/stores/store.ts
+++ client/src/lib/stores/store.ts
@@ -1,14 +1,11 @@
 import { createContext } from "react";
-import CounterStore from "./counterStore";
 import { UIStore } from "./uiStore";
 
 interface Store {
-  counterStore: CounterStore;
   uiStore: UIStore;
 }
 
 export const store: Store = {
-  counterStore: new CounterStore(),
   uiStore: new UIStore(),
 };
 
