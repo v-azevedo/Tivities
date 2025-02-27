7081007f5fbdd2425e16bef7b2979b70e73ac3cb
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-27 16:28:56 -0300

    feat: implement the handle result method

--- src/API/Controllers/ActivitiesController.cs
+++ src/API/Controllers/ActivitiesController.cs
@@ -23,23 +23,19 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<string>> CreateActivity(CreateActivityDto activityDto)
         {
-            return await Mediator.Send(new CreateActivity.Command { ActivityDto = activityDto });
+            return HandleResult(await Mediator.Send(new CreateActivity.Command { ActivityDto = activityDto }));
         }
 
         [HttpPut]
         public async Task<ActionResult> EditActivity(Activity activity)
         {
-            await Mediator.Send(new EditActivity.Command { Activity = activity });
-
-            return NoContent();
+            return HandleResult(await Mediator.Send(new EditActivity.Command { Activity = activity }));
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteActivity(string id)
         {
-            await Mediator.Send(new DeleteActivity.Command { Id = id });
-
-            return Ok();
+            return HandleResult(await Mediator.Send(new DeleteActivity.Command { Id = id }));
         }
     }
 }
