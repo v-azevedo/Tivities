a1e18eff07f98cce780d23a8d053779ffa87ea37
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-02-27 16:27:58 -0300

    feat: add error handling with the result class

--- src/Application/Activities/Commands/CreateActivity.cs
+++ src/Application/Activities/Commands/CreateActivity.cs
@@ -1,4 +1,5 @@
 using Application.Activities.DTOs;
+using Application.Core;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -8,14 +9,14 @@ namespace Application.Activities.Commands;
 
 public class CreateActivity
 {
-    public class Command : IRequest<string>
+    public class Command : IRequest<Result<string>>
     {
         public required CreateActivityDto ActivityDto { get; set; }
     }
 
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, string>
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<string>>
     {
-        public async Task<string> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<string>> Handle(Command request, CancellationToken cancellationToken)
         {
             var activity = mapper.Map<Activity>(request.ActivityDto);
 
@@ -23,7 +24,11 @@ public class CreateActivity
 
             await context.SaveChangesAsync(cancellationToken);
 
-            return activity.Id;
+            var result = await context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (!result) return Result<string>.Failure("Failed to create the activity", 400);
+
+            return Result<string>.Success(activity.Id);
         }
     }
 }
--- src/Application/Activities/Commands/EditActivity.cs
+++ src/Application/Activities/Commands/EditActivity.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -7,23 +8,28 @@ namespace Application.Activities.Commands;
 
 public class EditActivity
 {
-    public class Command : IRequest
+    public class Command : IRequest<Result<Unit>>
     {
         public required Activity Activity { get; set; }
 
 
     }
 
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command>
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<Unit>>
     {
-        public async Task Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
-            var activity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken)
-                ?? throw new Exception("Activity not found");
+            var activity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken);
+
+            if (activity is null) return Result<Unit>.Failure("Activity not found", 404);
 
             mapper.Map(request.Activity, activity);
 
-            await context.SaveChangesAsync(cancellationToken);
+            var result = await context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (!result) return Result<Unit>.Failure("Failed to update the activity", 400);
+
+            return Result<Unit>.Success(Unit.Value);
         }
     }
 }
