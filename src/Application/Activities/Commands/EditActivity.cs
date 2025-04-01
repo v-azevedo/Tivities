3b242694d6cc1ba5c504901e7b4ef14ebd7bcbae
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-01 15:53:04 -0300

    refactor: update property for command to match

--- src/Application/Activities/Commands/EditActivity.cs
+++ src/Application/Activities/Commands/EditActivity.cs
@@ -1,6 +1,6 @@
+using Application.Activities.DTOs;
 using Application.Core;
 using AutoMapper;
-using Domain;
 using MediatR;
 using Persistence;
 
@@ -10,20 +10,18 @@ public class EditActivity
 {
     public class Command : IRequest<Result<Unit>>
     {
-        public required Activity Activity { get; set; }
-
-
+        public required EditActivityDto ActivityDto { get; set; }
     }
 
     public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<Unit>>
     {
         public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
-            var activity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken);
+            var activity = await context.Activities.FindAsync([request.ActivityDto.Id], cancellationToken);
 
             if (activity is null) return Result<Unit>.Failure("Activity not found", 404);
 
-            mapper.Map(request.Activity, activity);
+            mapper.Map(request.ActivityDto, activity);
 
             var result = await context.SaveChangesAsync(cancellationToken) > 0;
 
