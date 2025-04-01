4b966b2c7a6355b479c979f38e67ae99d6ca1501
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-01 16:32:08 -0300

    feat: add toast container

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router";
 import { router } from "./app/router/Routes";
+import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
