ce54a24c6982c000063d3aaa15517e88995f8f05
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-02 14:52:11 -0300

    chore: add style sheet for toastify

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "react-toastify/ReactToastify.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
