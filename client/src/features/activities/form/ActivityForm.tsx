68447669524325c55b8251051a0a6e4e62dbdc9f
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-03 15:42:45 -0300

    feat: update fields to use custom text input with validation

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,35 +1,32 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import { FormEvent } from "react";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useForm } from "react-hook-form";
+
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { useNavigate, useParams } from "react-router";
+import {
+  activitySchema,
+  ActivitySchema,
+} from "../../../lib/schemas/activitySchema";
+import TextInput from "../../../app/shared/components/TextInput";
 
 const ActivityForm = () => {
+  const { reset, handleSubmit, control } = useForm<ActivitySchema>({
+    mode: "onTouched",
+    resolver: zodResolver(activitySchema),
+  });
+
   const { id } = useParams();
   const { updateActivity, createActivity, activity, isLoadingActivity } =
     useActivities(id);
-  const navigate = useNavigate();
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  useEffect(() => {
+    if (activity) reset(activity);
+  }, [activity, reset]);
 
-    const formData = new FormData(event.currentTarget);
-
-    const data: { [key: string]: FormDataEntryValue } = {};
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    if (activity) {
-      data.id = activity.id;
-      await updateActivity.mutateAsync(data as unknown as Activity);
-      navigate(`/activities/${activity.id}`);
-    } else {
-      createActivity.mutate(data as unknown as Activity, {
-        onSuccess: (id) => {
-          navigate(`/activities/${id}`);
-        },
-      });
-    }
+  const onSubmit = (data: ActivitySchema) => {
+    console.log(data);
   };
 
   if (isLoadingActivity) return <Typography>Loading...</Typography>;
@@ -41,36 +38,23 @@ const ActivityForm = () => {
       </Typography>
       <Box
         component="form"
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         display="flex"
         flexDirection="column"
         gap={3}
       >
-        <TextField name="title" label="Title" defaultValue={activity?.title} />
-        <TextField
-          name="description"
+        <TextInput label="Title" control={control} name="title" />
+        <TextInput
           label="Description"
-          defaultValue={activity?.description}
+          control={control}
+          name="description"
           multiline
           rows={3}
         />
-        <TextField
-          name="category"
-          label="Category"
-          defaultValue={activity?.category}
-        />
-        <TextField
-          name="date"
-          label="Date"
-          type="date"
-          defaultValue={
-            activity?.date
-              ? new Date(activity.date).toISOString().split("T")[0]
-              : new Date().toISOString().split("T")[0]
-          }
-        />
-        <TextField name="city" label="City" defaultValue={activity?.city} />
-        <TextField name="venue" label="Venue" defaultValue={activity?.venue} />
+        <TextInput label="Category" control={control} name="category" />
+        <TextInput label="Date" control={control} name="date" />
+        <TextInput label="City" control={control} name="city" />
+        <TextInput label="Venue" control={control} name="venue" />
         <Box display="flex" justifyContent="end" gap={3}>
           <Button color="inherit">Cancel</Button>
           <Button
