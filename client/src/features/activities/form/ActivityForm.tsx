04cb104032ead650c83d12df78b16d1ecc6fe5ad
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-04 18:45:48 -0300

    feat: update form to use location input

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Paper, Typography } from "@mui/material";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useForm } from "react-hook-form";
 
 import { categoryOptions } from "./categoryOptions";
@@ -13,23 +13,50 @@ import {
 import TextInput from "../../../app/shared/components/TextInput";
 import SelectInput from "../../../app/shared/components/SelectInput";
 import DateTImeInput from "../../../app/shared/components/DateTImeInput";
+import LocationInput from "../../../app/shared/components/LocationInput";
 
 const ActivityForm = () => {
   const { reset, handleSubmit, control } = useForm<ActivitySchema>({
     mode: "onTouched",
     resolver: zodResolver(activitySchema),
   });
+  const navigate = useNavigate();
 
   const { id } = useParams();
   const { updateActivity, createActivity, activity, isLoadingActivity } =
     useActivities(id);
 
   useEffect(() => {
-    if (activity) reset(activity);
+    if (activity)
+      reset({
+        ...activity,
+        location: {
+          city: activity.city,
+          venue: activity.venue,
+          latitude: activity.latitude,
+          longitude: activity.longitude,
+        },
+      });
   }, [activity, reset]);
 
-  const onSubmit = (data: ActivitySchema) => {
-    console.log(data);
+  const onSubmit = async (data: ActivitySchema) => {
+    const { location, ...rest } = data;
+    const flattenedData = { ...rest, ...location };
+
+    try {
+      if (activity) {
+        updateActivity.mutate(
+          { ...activity, ...flattenedData },
+          { onSuccess: () => navigate(`/activities/${activity.id}`) }
+        );
+      } else {
+        createActivity.mutate(flattenedData as Activity, {
+          onSuccess: (id) => navigate(`/activities/${id}`),
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (isLoadingActivity) return <Typography>Loading...</Typography>;
@@ -54,15 +81,20 @@ const ActivityForm = () => {
           multiline
           rows={3}
         />
-        <SelectInput
-          items={categoryOptions}
-          label="Category"
+        <Box sx={{ display: "flex", gap: 3 }}>
+          <SelectInput
+            items={categoryOptions}
+            label="Category"
+            control={control}
+            name="category"
+          />
+          <DateTImeInput label="Date" control={control} name="date" />
+        </Box>
+        <LocationInput
           control={control}
-          name="category"
+          label="Enter the location"
+          name="location"
         />
-        <DateTImeInput label="Date" control={control} name="date" />
-        <TextInput label="City" control={control} name="city" />
-        <TextInput label="Venue" control={control} name="venue" />
         <Box display="flex" justifyContent="end" gap={3}>
           <Button color="inherit">Cancel</Button>
           <Button
