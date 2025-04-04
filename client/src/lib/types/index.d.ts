3105d86b81665c5beaf3b4f4d5462bc08eaafca0
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-04 18:43:11 -0300

    feat: add types for the location iq responses

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -10,3 +10,34 @@ type Activity = {
   latitude: number;
   longitude: number;
 };
+
+type LocationIQSuggestion = {
+  place_id: string;
+  osm_id: string;
+  osm_type: string;
+  licence: string;
+  lat: string;
+  lon: string;
+  boundingbox: string[];
+  class: string;
+  type: string;
+  display_name: string;
+  display_place: string;
+  display_address: string;
+  address: LocationIQAddress;
+};
+
+type LocationIQAddress = {
+  name: string;
+  road?: string;
+  neighbourhood?: string;
+  suburb?: string;
+  town?: string;
+  village?: string;
+  city?: string;
+  county: string;
+  state: string;
+  postcode?: string;
+  country: string;
+  country_code: string;
+};
