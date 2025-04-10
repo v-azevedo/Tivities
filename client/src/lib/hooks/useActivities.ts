7db264480a6bd89a80f99e5292784579e76c7531
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-10 14:52:41 -0300

    feat: update hook to check for user before fetching activities

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,18 +1,20 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useLocation } from "react-router";
+import { useAccount } from "./useAccount";
 
 export const useActivities = (id?: string) => {
   const queryClient = useQueryClient();
+  const { currentUser } = useAccount();
   const location = useLocation();
 
-  const { data: activities, isPending } = useQuery({
+  const { data: activities, isLoading } = useQuery({
     queryKey: ["activities"],
     queryFn: async () => {
       const response = await agent.get<Activity[]>("/activities");
       return response.data;
     },
-    enabled: !id && location.pathname === "/activities",
+    enabled: !id && location.pathname === "/activities" && !!currentUser,
     staleTime: 1000 * 60 * 5,
   });
 
@@ -22,7 +24,7 @@ export const useActivities = (id?: string) => {
       const response = await agent.get<Activity>(`/activities/${id}`);
       return response.data;
     },
-    enabled: !!id,
+    enabled: !!id && !!currentUser,
   });
 
   const updateActivity = useMutation({
@@ -56,7 +58,7 @@ export const useActivities = (id?: string) => {
   return {
     activities,
     activity,
-    isPending,
+    isLoading,
     isLoadingActivity,
     updateActivity,
     createActivity,
