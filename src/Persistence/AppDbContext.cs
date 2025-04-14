09430fe4ae00303cae2f685d11f2b1b65639ff68
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-14 14:51:54 -0300

    feat: add configuration for attendees jointed table

--- src/Persistence/AppDbContext.cs
+++ src/Persistence/AppDbContext.cs
@@ -6,5 +6,25 @@ namespace Persistence;
 
 public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
-    public DbSet<Activity> Activities { get; set; }
+    public required DbSet<Activity> Activities { get; set; }
+    public required DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ActivityAttendee>(x => x.HasKey(a => new { a.ActivityId, a.UserId })); // Primary Key
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.Activities)
+            .HasForeignKey(x => x.UserId);
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(x => x.Activity)
+            .WithMany(x => x.Attendees)
+            .HasForeignKey(x => x.ActivityId);
+    }
 }
