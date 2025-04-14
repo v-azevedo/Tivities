619614f884a3261aa82a6510614c1e20d6b76dc3
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-14 14:51:21 -0300

    feat: create joined table to activities attendees

--- src/Domain/Activity.cs
+++ src/Domain/Activity.cs
@@ -5,24 +5,18 @@ namespace Domain;
 public class Activity
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
-
     public required string Title { get; set; }
-
     public DateTime Date { get; set; }
-
     public required string Description { get; set; }
-
     public required string Category { get; set; }
-
     public bool IsCancelled { get; set; }
-
 
     // Location properties
     public required string City { get; set; }
-
     public required string Venue { get; set; }
-
     public double Latitude { get; set; }
-
     public double Longitude { get; set; }
+
+    // Navigation properties
+    public ICollection<ActivityAttendee> Attendees { get; set; } = [];
 }
--- src/Domain/User.cs
+++ src/Domain/User.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain;
@@ -8,4 +7,7 @@ public class User : IdentityUser
     public string? DisplayName { get; set; }
     public string? Bio { get; set; }
     public string? ImageUrl { get; set; }
+
+    // Navigation properties
+    public ICollection<ActivityAttendee> Activities { get; set; } = [];
 }
