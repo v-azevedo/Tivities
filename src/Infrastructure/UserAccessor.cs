ee6d79acd2d73d73c0e1da873cb85b8a10d0628d
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-14 16:21:50 -0300

    feat: create accessor to return the logged in user

---
+++ src/Infrastructure/UserAccessor.cs
@@ -0,0 +1,23 @@
+using System.Security.Claims;
+using Application.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Persistence;
+
+namespace Infrastructure;
+
+public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext dbContext)
+    : IUserAccessor
+{
+    public string GetUsedId()
+    {
+        return httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("No user found");
+    }
+
+    public async Task<User> GetUserAsync()
+    {
+        return await dbContext.Users.FindAsync(GetUsedId())
+            ?? throw new UnauthorizedAccessException("No user is logged in");
+    }
+}
