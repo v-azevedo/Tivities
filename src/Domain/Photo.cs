d8b1f469033e09b81c7f2cf73ad0b83994e5d1f5
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-17 12:34:03 -0300

    fix: add missing references to the owner

--- src/Domain/Photo.cs
+++ src/Domain/Photo.cs
@@ -5,4 +5,8 @@ public class Photo
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string Url { get; set; }
     public required string PublicId { get; set; }
+
+    // Navigation
+    public string? UserId { get; set; }
+    public User User { get; set; } = null!;
 }
