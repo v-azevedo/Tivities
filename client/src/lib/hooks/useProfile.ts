356b3f387f9080647c0a71727ccc8a64c5cb7671
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-23 16:15:38 -0300

    feat: add mutation to upload photo

--- client/src/lib/hooks/useProfile.ts
+++ client/src/lib/hooks/useProfile.ts
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useMemo } from "react";
 
@@ -23,6 +23,37 @@ export const UseProfile = (id?: string) => {
     enabled: !!id,
   });
 
+  const uploadPhoto = useMutation({
+    mutationFn: async (file: Blob) => {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await agent.post("/profiles/add-photo", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      return response.data;
+    },
+    onSuccess: async (photo: Photo) => {
+      await queryClient.invalidateQueries({
+        queryKey: ["photos", id],
+      });
+      queryClient.setQueryData(["user"], (data: User) => {
+        if (!data) return data;
+        return {
+          ...data,
+          imageUrl: data.imageUrl ?? photo.url,
+        };
+      });
+      queryClient.setQueryData(["profile", id], (data: Profile) => {
+        if (!data) return data;
+        return {
+          ...data,
+          imageUrl: data.imageUrl ?? photo.url,
+        };
+      });
+    },
+  });
+
   const isCurrentUser = useMemo(() => {
     return id === queryClient.getQueryData<User>(["user"])?.id;
   }, [id, queryClient]);
@@ -33,5 +64,6 @@ export const UseProfile = (id?: string) => {
     photos,
     loadingPhotos,
     isCurrentUser,
+    uploadPhoto,
   };
 };
