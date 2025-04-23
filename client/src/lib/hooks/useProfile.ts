013262cd673f621a55dc1faff89d5f024b4f7c01
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-23 15:13:50 -0300

    feat: add memoized var to query the current user

--- client/src/lib/hooks/useProfile.ts
+++ client/src/lib/hooks/useProfile.ts
@@ -1,13 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
+import { useMemo } from "react";
 
 export const UseProfile = (id?: string) => {
+  const queryClient = useQueryClient();
+
   const { data: profile, isLoading: loadingProfile } = useQuery<Profile>({
     queryKey: ["profile", id],
     queryFn: async () => {
       const response = await agent.get<Profile>(`/profiles/${id}`);
       return response.data;
     },
+    enabled: !!id,
   });
 
   const { data: photos, isLoading: loadingPhotos } = useQuery<Photo[]>({
@@ -16,12 +20,18 @@ export const UseProfile = (id?: string) => {
       const response = await agent.get<Photo[]>(`/profiles/${id}/photos`);
       return response.data;
     },
+    enabled: !!id,
   });
+
+  const isCurrentUser = useMemo(() => {
+    return id === queryClient.getQueryData<User>(["user"])?.id;
+  }, [id, queryClient]);
 
   return {
     profile,
     loadingProfile,
     photos,
     loadingPhotos,
+    isCurrentUser,
   };
 };
