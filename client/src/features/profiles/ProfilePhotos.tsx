f7cbd8fb4772e10363f71d69fd38245a10593791
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-04-23 15:14:20 -0300

    feat: add button to add photo onl y for owner of profile

--- client/src/features/profiles/ProfilePhotos.tsx
+++ client/src/features/profiles/ProfilePhotos.tsx
@@ -1,10 +1,18 @@
 import { useParams } from "react-router";
 import { UseProfile } from "../../lib/hooks/useProfile";
-import { ImageList, ImageListItem, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function ProfilePhotos() {
   const { id } = useParams();
-  const { loadingPhotos, photos } = UseProfile(id);
+  const { loadingPhotos, photos, isCurrentUser } = UseProfile(id);
+  const [editMode, setEditMode] = useState(false);
 
   if (loadingPhotos) return <Typography>Loading photos...</Typography>;
 
@@ -12,23 +20,36 @@ export default function ProfilePhotos() {
     return <Typography>No photos found for this user.</Typography>;
 
   return (
-    <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
-      {photos.map((photo) => (
-        <ImageListItem key={photo.id}>
-          <img
-            srcSet={`${photo.url.replace(
-              "/upload/",
-              "/upload/w_164,h_164,c_fill,f_auto,dpr_2,g_face/"
-            )}`}
-            src={`${photo.url.replace(
-              "/upload/",
-              "/upload/w_164,h_164,c_fill,f_auto,g_face/"
-            )}`}
-            alt="user profile image"
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <Box>
+      {isCurrentUser && (
+        <Box>
+          <Button onClick={() => setEditMode(!editMode)}>
+            {editMode ? "Cancel" : "Add photo"}
+          </Button>
+        </Box>
+      )}
+      {editMode ? (
+        <div>Photo widget goes here</div>
+      ) : (
+        <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
+          {photos.map((photo) => (
+            <ImageListItem key={photo.id}>
+              <img
+                srcSet={`${photo.url.replace(
+                  "/upload/",
+                  "/upload/w_164,h_164,c_fill,f_auto,dpr_2,g_face/"
+                )}`}
+                src={`${photo.url.replace(
+                  "/upload/",
+                  "/upload/w_164,h_164,c_fill,f_auto,g_face/"
+                )}`}
+                alt="user profile image"
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      )}
+    </Box>
   );
 }
