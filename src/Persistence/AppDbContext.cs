118a579be41fc298e0930a18b3496d93e0c25142
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-12 15:29:31 -0300

    feat: create new migration to add comments entity

--- src/Persistence/AppDbContext.cs
+++ src/Persistence/AppDbContext.cs
@@ -9,6 +9,7 @@ public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(op
     public required DbSet<Activity> Activities { get; set; }
     public required DbSet<ActivityAttendee> ActivityAttendees { get; set; }
     public required DbSet<Photo> Photos { get; set; }
+    public required DbSet<Comment> Comments { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
