2eb3502e7be171c10466100b80c2b7372bcd15c2
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-15 15:02:06 -0300

    feat: update mapping profile to include following

--- src/Application/Core/MappingProfiles.cs
+++ src/Application/Core/MappingProfiles.cs
@@ -9,6 +9,8 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
+        string? currentUserId = null;
+
         CreateMap<Activity, Activity>();
         CreateMap<CreateActivityDto, Activity>();
         CreateMap<EditActivityDto, Activity>();
@@ -21,8 +23,16 @@ public class MappingProfiles : Profile
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.User.DisplayName))
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.User.Bio))
             .ForMember(d => d.ImageUrl, o => o.MapFrom(s => s.User.ImageUrl))
-            .ForMember(d => d.Id, o => o.MapFrom(s => s.User.Id));
-        CreateMap<User, UserProfile>();
+            .ForMember(d => d.Id, o => o.MapFrom(s => s.User.Id))
+            .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.User.Followers.Count))
+            .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.User.Followings.Count))
+            .ForMember(d => d.Following, o => o.MapFrom(
+                s => s.User.Followers.Any(X => X.Observer.Id == currentUserId)));
+        CreateMap<User, UserProfile>()
+            .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+            .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count))
+            .ForMember(d => d.Following, o => o.MapFrom(
+                s => s.Followers.Any(X => X.Observer.Id == currentUserId)));
         CreateMap<Comment, CommentDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.User.DisplayName))
             .ForMember(d => d.UserId, o => o.MapFrom(s => s.User.UserName))
