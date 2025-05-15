df69af8f8e0ad06500325e9af10b404f80f4a5d5
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-15 15:27:36 -0300

    feat: add ability to update following

--- client/src/features/profiles/ProfileHeader.tsx
+++ client/src/features/profiles/ProfileHeader.tsx
@@ -9,12 +9,15 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
+import { useParams } from "react-router";
+import { UseProfile } from "../../lib/hooks/useProfile";
 
-type Props = {
-  profile: Profile;
-};
+export default function ProfileHeader() {
+  const { id } = useParams();
+  const { isCurrentUser, profile, updateFollowing } = UseProfile(id);
 
-export default function ProfileHeader({ profile }: Props) {
+  if (!profile) return null;
+
   return (
     <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
       <Grid2 container spacing={2}>
@@ -50,14 +53,20 @@ export default function ProfileHeader({ profile }: Props) {
                 <Typography variant="h3">{profile.followingCount}</Typography>
               </Box>
             </Box>
-            <Divider sx={{ width: "100%" }} />
-            <Button
-              fullWidth
-              variant="outlined"
-              color={profile.following ? "error" : "success"}
-            >
-              {profile.following ? "Unfollow" : "Follow"}
-            </Button>
+            {!isCurrentUser && (
+              <>
+                <Divider sx={{ width: "100%" }} />
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  color={profile.following ? "error" : "success"}
+                  onClick={() => updateFollowing.mutate()}
+                  disabled={updateFollowing.isPending}
+                >
+                  {profile.following ? "Unfollow" : "Follow"}
+                </Button>
+              </>
+            )}
           </Stack>
         </Grid2>
       </Grid2>
