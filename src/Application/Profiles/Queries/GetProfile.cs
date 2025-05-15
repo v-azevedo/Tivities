fdbb8b8e5b600cee4fd81ce6e74896b5e042b85f
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-15 15:03:46 -0300

    refactor: update projection to pass the current user id

--- src/Application/Profiles/Queries/GetProfile.cs
+++ src/Application/Profiles/Queries/GetProfile.cs
@@ -1,4 +1,5 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
@@ -14,12 +15,13 @@ public class GetProfile
         public required string UserId { get; set; }
     }
 
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Query, Result<UserProfile>>
+    public class Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor) : IRequestHandler<Query, Result<UserProfile>>
     {
         public async Task<Result<UserProfile>> Handle(Query request, CancellationToken cancellationToken)
         {
             var profile = await context.Users
-                .ProjectTo<UserProfile>(mapper.ConfigurationProvider)
+                .ProjectTo<UserProfile>(mapper.ConfigurationProvider,
+                    new { currentUserId = userAccessor.GetUsedId() })
                 .SingleOrDefaultAsync(x => x.Id == request.UserId, cancellationToken);
 
             return profile is null
