ad2920a6edceb4533515342ff823279566441e55
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-16 15:02:14 -0300

    feat: update query to use queryable queries for paging

--- src/Application/Activities/Queries/GetActivityList.cs
+++ src/Application/Activities/Queries/GetActivityList.cs
@@ -1,4 +1,5 @@
 using Application.Activities.DTOs;
+using Application.Core;
 using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -10,16 +11,55 @@ namespace Application.Activities.Queries;
 
 public class GetActivityList
 {
-    public class Query : IRequest<List<ActivityDto>> { }
+    private const int MaxPageSize = 50;
 
-    public class Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor) : IRequestHandler<Query, List<ActivityDto>>
+    public class Query : IRequest<Result<PagedList<ActivityDto, DateTime?>>>
     {
-        public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
+        public DateTime? Cursor { get; set; }
+
+        private int _pageSize = 3;
+        public int PageSize
         {
-            return await context.Activities
+            get { return _pageSize; }
+            set { _pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+    }
+
+    public class Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor) :
+        IRequestHandler<Query, Result<PagedList<ActivityDto, DateTime?>>>
+    {
+        public async Task<Result<PagedList<ActivityDto, DateTime?>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var query = context.Activities
+                .OrderBy(x => x.Date)
+                .AsQueryable();
+
+            if (request.Cursor.HasValue)
+            {
+                query = query.Where(x => x.Date >= request.Cursor.Value);
+            }
+
+            var activities = await query
+                .Take(request.PageSize + 1)
                 .ProjectTo<ActivityDto>(mapper.ConfigurationProvider,
                     new { currentUserId = userAccessor.GetUsedId() })
                 .ToListAsync(cancellationToken);
+
+            DateTime? nextCursor = null;
+
+            if (activities.Count > request.PageSize)
+            {
+                nextCursor = activities.Last().Date;
+                activities.RemoveAt(activities.Count - 1);
+            }
+
+            return Result<PagedList<ActivityDto, DateTime?>>.Success(
+                new PagedList<ActivityDto, DateTime?>
+                {
+                    Items = activities,
+                    NextCursor = nextCursor
+                }
+            );
         }
     }
 }
