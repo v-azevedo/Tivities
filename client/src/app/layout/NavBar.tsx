55e81a9aa98812e43f19c80399f619020b5f76fe
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-16 16:03:25 -0300

    style: fix navbar to top, change loading icon

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -2,8 +2,8 @@ import { Group } from "@mui/icons-material";
 import {
   AppBar,
   Box,
+  CircularProgress,
   Container,
-  LinearProgress,
   MenuItem,
   Toolbar,
   Typography,
@@ -22,7 +22,7 @@ const NavBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
-        position="relative"
+        position="fixed"
         sx={{
           backgroundImage:
             "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
@@ -37,9 +37,29 @@ const NavBar = () => {
                 sx={{ display: "flex", gap: 2 }}
               >
                 <Group fontSize="large" />
-                <Typography variant="h4" fontWeight="bold">
+                <Typography
+                  sx={{ position: "relative" }}
+                  variant="h4"
+                  fontWeight="bold"
+                >
                   Tivities
                 </Typography>
+                <Observer>
+                  {() =>
+                    uiStore.isLoading ? (
+                      <CircularProgress
+                        size={20}
+                        thickness={7}
+                        sx={{
+                          color: "white",
+                          position: "absolute",
+                          top: "30%",
+                          left: "105%",
+                        }}
+                      />
+                    ) : null
+                  }
+                </Observer>
               </MenuItem>
             </Box>
             <Box sx={{ display: "flex" }}>
@@ -58,22 +78,6 @@ const NavBar = () => {
             </Box>
           </Toolbar>
         </Container>
-        <Observer>
-          {() =>
-            uiStore.isLoading ? (
-              <LinearProgress
-                color="secondary"
-                sx={{
-                  position: "absolute",
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  height: 4,
-                }}
-              />
-            ) : null
-          }
-        </Observer>
       </AppBar>
     </Box>
   );
