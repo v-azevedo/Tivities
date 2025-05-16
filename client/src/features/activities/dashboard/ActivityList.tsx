00f7a593226f32383755de8bc953bafb93775419
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-16 16:12:18 -0300

    feat: implement infinite scroll pagination

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,10 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { Fragment } from "react/jsx-runtime";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const ActivityList = () => {
-  const { activitiesGroup, isLoading } = useActivities();
+  const { activitiesGroup, isLoading, hasNextPage, fetchNextPage } =
+    useActivities();
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   if (isLoading) return <Typography>Loading...</Typography>;
 
@@ -13,11 +24,17 @@ const ActivityList = () => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {activitiesGroup.pages.map((activities, index) => (
-        <Fragment key={index}>
+        <Box
+          key={index}
+          ref={index === activitiesGroup.pages.length - 1 ? ref : null}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+        >
           {activities.items.map((activity) => (
             <ActivityCard key={activity.id} activity={activity} />
           ))}
-        </Fragment>
+        </Box>
       ))}
     </Box>
   );
