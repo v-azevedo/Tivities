ad3fc632fd9acc1632c57afcb742b84d8bba7756
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2025-05-16 16:12:02 -0300

    refactor: remove button to load more activities

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,25 +1,13 @@
-import { Button, Grid2 } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityFilters from "./ActivityFilters";
-import { useActivities } from "../../../lib/hooks/useActivities";
 
 const ActivityDashboard = () => {
-  const { isFetchingNextPage, fetchNextPage, hasNextPage } = useActivities();
-
   return (
     <Grid2 container spacing={3}>
       <Grid2 size={8}>
         <ActivityList />
-        <Button
-          onClick={() => fetchNextPage()}
-          sx={{ my: 2, float: "right" }}
-          variant="contained"
-          disabled={!hasNextPage || isFetchingNextPage}
-        >
-          Load more
-        </Button>
       </Grid2>
-      {/* TODO: Implement activities filters */}
       <Grid2
         size={4}
         sx={{
